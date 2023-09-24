ef00ec0686cd73e64c10e67a9fcec29752d7f1fd
Najma Saeed <najmasy20@gmail.com>
2023-09-24 20:19:40 +0100

    js problems with 4 and 5

--- js/app.js
+++ js/app.js
@@ -88,15 +88,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4];
 
 function sumArray(sumArr) {
-  //eslint-disable-line
+  let sum1 = 0;
+
+  for (let i = 0; i < testArray.length; i++) {
+    sum1 += testArray[i];
+  }
+
+  let string1 = `${testArray[0]} and ${testArray[1]} and ${testArray[2]} was passed in as an array of numbers, and ${sum1} is their sum.`;
+
+  return [sum1, string1];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -112,11 +120,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-  //eslint-disable-line
+  let multiply1 = 0;
+
+  for (let i = 0; i < testArray.length; i++) {
+    multiply1 += testArray[i];
+  }
+
+  let string2 = `The numbers ${testArray[0]},${testArray[1]}, ${testArray[2]} have a product of ${multiply1}.`;
+
+  return [multiply1, string2];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
@@ -135,9 +151,9 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 This function should be dynamic, accepting an array of any length.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
-/*
+
 // Write your code here
-let testDynamicArray = [1, 2, 3, 4, 5]; 
+let testDynamicArray = [1, 2, 3, 4, 5];
 
 function multiplyAnyArray(dynamicArray) {
   let product = 1;
@@ -159,5 +175,3 @@ return [product, string];
 testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
-
-*/
